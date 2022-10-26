1dbc73f196f2341fe07368e143e1068fa6dc0f1a
Nguyen Khai 22 <nguyenvankhai161220@gmail.com>
2022-10-27 02:25:46 +0700

    done

--- access/js/functions.js
+++ access/js/functions.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 	$(document).on('click', '.nav-item-icon-search', function () {
 		$('.nav-item-icon-search').addClass("is-active");
 		$('.navbar').addClass("nav-is-active");
@@ -9,10 +9,12 @@ jQuery(document).ready(function($) {
 		$('.nav-item-icon-search').removeClass("is-active");
 		$(".search").removeClass("is-active");
 		$('.navbar').removeClass("nav-is-active");
+		$(".close-button").removeClass("is-active");
 	});
 	$(document).on('click', '.menu-item', function () {
 		$('.nav-item-icon-search').removeClass("is-active");
 		$(".search").removeClass("is-active");
 		$('.navbar').removeClass("nav-is-active");
+		$(".close-button").removeClass("is-active");
 	});
 })
